1b47fea25ea9b61280ef6b0725349152f0b5a867
kares <self@kares.org>
2012-10-10 21:32:35 +0200

    require rubygems for tests (1.8 compatibility)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 begin
   require 'test/unit/version'
 rescue LoadError => e
+  require('rubygems') && retry
   fail "`bundle install` to install Test::Unit 2.x (#{e.inspect})"
 end
 $LOAD_PATH.unshift File.expand_path('../lib', File.dirname(__FILE__))
