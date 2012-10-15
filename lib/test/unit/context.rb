154037980c745af2d17dc10bed19abfb3fd1a16e
kares <self@kares.org>
2012-10-15 16:23:02 +0200

    a more sensible Test::Unit::Context implementation: - #context_name should not exist for base TestCases - better context class names - names should be unique (thus warn on duplicate definitions) - truly support name-less (anonymous) context definitions - add #context_list for TestCases (as well as context)

--- lib/test/unit/context.rb
+++ lib/test/unit/context.rb
@@ -6,17 +6,10 @@ module Test
   module Unit
     module Context
 
-      def context_name #:nodoc:
-        @context_name ||= ""
-        if superclass.respond_to?(:context_name)
-          return "#{superclass.context_name} #{@context_name}".gsub(/^\s+/, "")
-        end
+      def context_list
+        @context_list ||= []
       end
-
-      def context_name=(val) #:nodoc:
-        @context_name = val
-      end
-
+      
       # Add a context to a set of tests.
       # 
       #   context "A new account" do
@@ -41,28 +34,60 @@ module Test
       #     end
       #   end
       #
-      def context(name, &block)
-        klass = Class.new(self)
-        klass.context_name = name
-        # NOTE: make sure by default we run "inherited" setup/teardown hooks
-        # unless context code does re-define the hook method e.g. `def setup` 
-        # instead of using the `setup do` or the setup method marker syntax :
-        klass.class_eval do
-          def setup; super; end
-          def cleanup; super; end
-          def teardown; super; end
+      # Context should have unique names within a given scope, otherwise they 
+      # end-up being merged as if it where one single context declaration. 
+      # Anonymous (un-named) contexts are supported as well - contrary they 
+      # never get merged (a unique name is generated for each such context).
+      #
+      def context(name = nil, &block)
+        name ||= (Time.now.to_f * 1000).to_i
+        # context "created with defaults" ... 'ContextCreatedWithDefaults'
+        class_name = "Context#{Helpers.to_class_name(name.to_s)}"
+        if const_defined?(class_name)
+          klass = const_get(class_name)
+          if ( klass.superclass == self rescue nil )
+            warn "duplicate context definition with the name #{name.inspect} " << 
+                  "found at #{caller.first} and is going to get merged with " << 
+                  "the previous context definition"
+          else
+            raise "could not create a context with the name #{name.inspect} " << 
+                  "as a constant #{class_name} is already defined and is not " << 
+                  "another context definition"
+          end
+        else
+          klass = Class.new(self)
+          klass.extend ContextMethods
+          klass.context_name = name
+          # NOTE: make sure by default we run "inherited" setup/teardown hooks
+          # unless context code does re-define the hook method e.g. `def setup` 
+          # instead of using the `setup do` or the setup method marker syntax :
+          klass.class_eval do
+            def setup; super; end
+            def cleanup; super; end
+            def teardown; super; end
+          end
+          const_set(class_name, klass)
+          self.context_list << klass
         end
         klass.class_eval(&block)
-
-        #@@context_list << klass # make sure it's not GC-ed ?!
-        class_name = Helpers.to_class_name(name)
-        const_set("Test#{class_name}#{klass.object_id.abs}", klass)
         klass
       end
 
       %w( contexts group ).each { |m| alias_method m, :context }
       
-      #@@context_list = []
+      module ContextMethods # :nodoc:
+        
+        def context_name
+          if superclass.respond_to?(:context_name)
+            "#{superclass.context_name} #{@context_name}".gsub(/^\s+/, "")
+          else
+            @context_name
+          end
+        end
+        
+        def context_name=(name); @context_name=name; end
+        
+      end
       
     end
   end
--- test/test/unit/context_test.rb
+++ test/test/unit/context_test.rb
@@ -16,56 +16,171 @@ module Test::Unit
       assert self.class.respond_to? :contexts
     end
 
-    context "A new context" do
-      
-      context "when not nested" do
-        setup do
-          @context = 
-            Class.new(Test::Unit::TestCase).
-              context("When testing") do
-                def test_this_thing
-                  true
-                end
-              end
+    class Default < Test::Unit::TestCase
+      CONTEXT = context "When testing" do
+        def test_this_thing
+          true
         end
-
-        test "should set the context name" do
-          assert_equal "When testing", @context.context_name
-        end
-
-        test "should be a Test::Unit::TestCase" do
-          assert @context.ancestors.include?(Test::Unit::TestCase)
-        end
-        
       end
+      context "More testing" do
+        # @todo implement more tests here ...
+      end
+    end
 
-      context "when nested" do
-        
-        setup do
-          @context = self.class.context("and we're testing") do
-            def self.nested
-              @nested
-            end
+    setup do
+      @default_context = Default::CONTEXT
+    end
+    
+    test "[default context] sets the context name" do
+      assert_equal "When testing", @default_context.context_name
+    end
 
-            @nested = context "should be nested" do
-              def test_this_thing
-                true
-              end
-            end
-          end
+    test "[default context] is a Test::Unit::TestCase" do
+      assert @default_context.ancestors.include?(Test::Unit::TestCase)
+    end
+
+    test "[default context] is defived from the test class" do
+      assert_equal Default, @default_context.superclass
+    end
+    
+    test "[default context] reports among test case's context list" do
+      assert Default.respond_to?(:context_list)
+      assert_include Default.context_list, @default_context
+      assert_equal 2, Default.context_list.size
+    end
+    
+    test "has a (context name derived) class name" do
+      namespace = 'Test::Unit::TestContext::Default::'
+      assert_equal "#{namespace}ContextWhenTesting", @default_context.name
+    end
+
+    class Anonymous < Test::Unit::TestCase
+      CONTEXT = context do
+        def test_some_thing
+          true
         end
-
-        test "should set a nested context's name" do
-          assert_equal "A new context when nested and we're testing should be nested", @context.nested.context_name
+      end
+      context do
+        test 'another_thing' do
         end
-
-        test "should also be a Test::Unit::TestCase" do
-          assert @context.nested.ancestors.include?(Test::Unit::TestCase)
-        end
-        
+      end
+      context do
+        #
       end
     end
     
+    setup do
+      @anonymous_context = Anonymous::CONTEXT
+    end
+
+    test "[anonymous context] has a generated context name" do
+      assert_not_nil @anonymous_context.name
+    end
+    
+    test "[anonymous context] is defived from the test class" do
+      assert_equal Anonymous, @anonymous_context.superclass
+    end
+    
+    test "[anonymous context] reports among test case's context list" do
+      assert Anonymous.respond_to?(:context_list)
+      assert_include Anonymous.context_list, @anonymous_context
+      assert_equal 3, Anonymous.context_list.size
+    end
+    
+    test "[anonymous context] has a (context name derived) class name" do
+      namespace = 'Test::Unit::TestContext::Anonymous::'
+      context_name = @anonymous_context.context_name
+      assert_equal "#{namespace}Context#{context_name}", @anonymous_context.name
+    end
+    
+    class Nested < Test::Unit::TestCase
+      CONTEXT = context "and we're testing" do
+        def self.nested; @nested; end
+        @nested = context "should be nested" do
+          def test_a_thing
+            true
+          end
+        end
+      end
+    end
+
+    setup do
+      @parent_context = Nested::CONTEXT
+      @nested_context = Nested::CONTEXT.nested
+    end
+    
+    test "[nested context] sets a nested context name" do
+      assert_equal "and we're testing should be nested", @nested_context.context_name
+    end
+
+    test "[nested context] is also a Test::Unit::TestCase" do
+      assert @nested_context.ancestors.include?(Test::Unit::TestCase)
+    end
+
+    test "[nested context] is defived from the prev context class" do
+      assert_equal @parent_context, @nested_context.superclass
+    end
+    
+    test "[nested context] reports context list correctly" do
+      assert Nested.respond_to?(:context_list)
+      assert_equal 1, Nested.context_list.size
+      assert_equal 1, @parent_context.context_list.size
+      assert_equal 0, @nested_context.context_list.size
+    end
+    
+    test "[nested context] has a (context name derived) class name" do
+      namespace = 'Test::Unit::TestContext::Nested::'
+      assert_equal "#{namespace}ContextAndWeReTesting::ContextShouldBeNested", 
+                    @nested_context.name
+    end
+    
+    class Redefined < Test::Unit::TestCase
+      CONTEXT = context 42 do
+        def test_everything
+          true
+        end
+      end
+      @@warns = nil
+      def self.warn(message)
+        ( @@warns ||= [] ) << message
+        super
+      end
+      def self.warns; @@warns; end
+      def self.reset_warns; @@warns = nil; end
+    end
+
+    setup do
+      @redefined_context = Redefined::CONTEXT
+    end
+    
+    test "[redefined context] sets the context name" do
+      assert_equal 42, @redefined_context.context_name
+    end
+
+    test "[redefined context] warns when same context name used" do
+      assert_nil Redefined.warns
+      class Redefined
+        context 42 do
+          def test_something_else
+            assert true
+          end
+        end
+      end
+      assert_not_nil Redefined.warns
+      assert_equal 2, @redefined_context.instance_methods(false).grep(/test/).size
+      
+      Redefined.reset_warns
+      
+      assert_nil Redefined.warns
+      class Redefined
+        context '42' do # same class-name
+          def test_a_little_thing
+          end
+        end
+      end
+      assert_not_nil Redefined.warns
+      assert_equal 3, @redefined_context.instance_methods(false).grep(/test/).size
+    end
+    
   end
-  
 end
