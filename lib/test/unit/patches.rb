5578d1ebc4a05ff2accb7bb6814d91490b93f59d
kares <self@kares.org>
2015-02-02 09:02:57 +0100

    Test-Unit 3.0 compatibility (fixes #4)

--- lib/test/unit/patches.rb
+++ lib/test/unit/patches.rb
@@ -1,11 +1,17 @@
 require 'test/unit'
-require 'test/unit/testsuitecreator'
 
 module Test::Unit
+  unless const_defined? :TestSuiteCreator
+    begin
+      require 'test/unit/test-suite-creator' # TU 3.x
+    rescue LoadError
+      require 'test/unit/testsuitecreator' # <= 2.5.5
+    end
+  end
   TestSuiteCreator.class_eval do
 
     private
-
+    
     def collect_test_names
       methods = @test_case.public_instance_methods(true)
       test_case_super = @test_case.superclass
@@ -13,7 +19,8 @@ module Test::Unit
         methods -= test_case_super.public_instance_methods(true)
         test_case_super = test_case_super.superclass
       end
-      test_names = methods.map(&:to_s).find_all do |method_name|
+      methods.map!(&:to_s)
+      test_names = methods.find_all do |method_name|
         method_name =~ /^test./ or @test_case.attributes(method_name)[:test]
       end
       send("sort_test_names_in_#{@test_case.test_order}_order", test_names)
