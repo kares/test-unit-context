80f584b3ec9dedbb396d5bb309f642db796018e3
kares <self@kares.org>
2016-02-01 12:01:03 +0100

    puts test-unit version in tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,6 @@
 begin
   require 'test/unit/version'
+  puts "using Test::Unit::VERSION = #{Test::Unit::VERSION}" if $VERBOSE
 rescue LoadError => e
   require('rubygems') && retry
   fail "`bundle install` to install Test::Unit 2.x (#{e.inspect})"
